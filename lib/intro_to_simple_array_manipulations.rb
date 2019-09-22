dfa62e8d0810fa414b34443d1d8876f9b15f9eb0
Ricky dildy <rdildy92@gmail.com>
2019-09-22 19:23:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,9 @@ def superhero = ["auqaman", "batman", "flash"]
 next_superhero = "wonderwoman"
 #=> ["auqaman", "batman", "flash", "wonderwoman"]
 end 
-def superhero = 
+def superhero = ["auqaman", "batman", "flash"]
+  superhero.pop 
+  #=> "flash"
+  superhero
+  #=> ["auqaman", "batman"]
+end 
